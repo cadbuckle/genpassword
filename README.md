9d39838a327f69f2e9f258334647750a7458a279
Duncan B <duncanbuckle@gmail.com>
2024-01-04 20:17:19 +0000

    Updated Readme with screenshots. Added some corretions to JS

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# genpassword
+# Generate Password
+
+## Description
+
+This page runs a JavaScript that provides the user with the ability to generate a password. The generated password can then be cut from the page and pasted as required.
+
+## Table of Contents
+
+* [Links](#links)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Screenshots](#screenshots)
+* [License](#license)
+* [Features](#features)
+
+## Links
+
+- [Repo](https://github.com/cadbuckle/genpassword)
+- [Live](https://cadbuckle.github.io/genpassword/)
+
+## Installation
+
+N/A
+
+## Usage 
+
+After the page is displayed, clicking the "Generate Password" button will start the process.
+A confirmation prompt will initially be given to ensure the user wants to generate a password.
+If the user has confirmed they want a password, further individual prompts will be given to the user for:
+- Enter the maximum number of characters for the password. This must be from 8 to 128 characters.<br>
+The following 4 prompts will then be given to the user. At least one of them must be selected.
+- To confirm if the user wants to use lowercase characters.
+- To confirm if the user wants to use uppercase characters.
+- To confirm if the user wants to use numbers.
+- To confirm if the user wants to use special characters.<br>
+The system will then generate the password and then display it to the user. 
+
+## Screenshots
+### Initial Screen
+<img src="./images/Screen-01-InitialScreen.png" width=50% alt="password generator initial screen"></img>
+### Confirm Password Required
+<img alt="password generator confirmation" src="./images/Screen-02-ConfirmPasswordRequired.png" width=50%></img>
+### How Many Characters?
+<img alt="password generator how many characters prompt" src="./images/Screen-03-HowManyCharacters.png" width=50%></img>
+### Invalid Number of Characters
+<img alt="password generator invalid number of characters error" src="./images/Screen-03-InvalidNumberOfCharacters.png" width=50%></img>
+### Lowercase Required?
+<img alt="password generator lowercase required prompt" src="./images/Screen-04-LowercaseRequired.png" width=50%></img>
+### Uppercase Required?
+<img alt="password generator uppercase required prompt" src="./images/Screen-05-UppercaseRequired.png" width=50%></img>
+### Numbers Required?
+<img alt="password generator numbers required prompt" src="./images/Screen-06-NumbersRequired.png" width=50%></img>
+### Special Characters Required?
+<img alt="password generator special characters required prompt" src="./images/Screen-07-SpecialCharactersRequired.png" width=50%></img>
+### Must Select An Option Error
+<img alt="password generator no option selected error" src="./images/Screen-08-MustSelectAnOption.png" width=50%></img>
+### Generated Password
+<img alt="password generator generated password screen" src="./images/Screen-09-GeneratedPassword.png" width=50%></img>
+
+## License
+
+MIT License as per repository.
+
+## Features
+
+JavaScript, notably:
+- User entry/selection validation.
+- Arrays: reading and concatenation of.
+- Math: for randomisation of character selection.
+
+
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- js/script.js
+++ js/script.js
@@ -1,28 +1,7 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  "@",
-  "%",
-  "+",
-  "\\",
-  "/",
-  "'",
-  "!",
-  "#",
-  "$",
-  "^",
-  "?",
-  ":",
-  ",",
-  ")",
-  "(",
-  "}",
-  "{",
-  "]",
-  "[",
-  "~",
-  "-",
-  "_",
-  ".",
+  "@",  "%",  "+",  "\\",  "/",  "'",  "!",  "#",  "$",  "^",  "?",  ":",
+  ",",  ")",  "(",  "}",  "{",  "]",  "[",  "~",  "-",  "_",  ".",
 ];
 
 // Array of numeric characters to be included in password
@@ -30,62 +9,14 @@ var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+  "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",
+  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
+  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  
+  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z",
 ];
 
 // useable array based upon selected character options
@@ -96,9 +27,10 @@ var numChar = 0;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  
   // Confirm that user wants to generate a password
   let goPass = confirm("Do you wish to generate a password?");
+
+  // if goPass then user wants a password so prompt for options.
   if (goPass) {
     // Prompt user for password options
     let numCharValid = false;
@@ -132,15 +64,19 @@ function getPasswordOptions() {
     }
 
     // build array of useable characters available for the password based upon user selection
-    if (optLower ) {
-      useableCharacters = useableCharacters.concat(lowerCasedCharacters);
-    };
+    // add lowercase characters if selected
+    if (optLower) {
+      useableCharacters = useableCharacters.concat(lowerCasedCharacters,lowerCasedCharacters);
+    }
+    // add uppercase characters if selected
     if (optUpper) {
-      useableCharacters = useableCharacters.concat(upperCasedCharacters);
-    };
+      useableCharacters = useableCharacters.concat(upperCasedCharacters,upperCasedCharacters);
+    }
+    // add numerics if selected
     if (optNumeric) {
       useableCharacters = useableCharacters.concat(numericCharacters);
     }
+    // add special characters if selected
     if (optSpecial) {
       useableCharacters = useableCharacters.concat(specialCharacters);
     }
@@ -148,24 +84,38 @@ function getPasswordOptions() {
 }
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+function getRandom() {
   // initialise local variable to hold all selected characters.
   let builtPassword = "";
 
-  // Iterate a loop <numChar> times to select a randamon useable character
+  // Iterate a loop <numChar> times to select a random useable character
   for (let a = 1; a <= numChar; a++) {
-    let randomCharacter = arr[Math.floor(Math.random() * arr.length)];
+    // select a random character from the useablechar array based upon it's length
+    let randomCharacter = useableCharacters[Math.floor(Math.random() * useableCharacters.length)];
+    // add selected character to local variable
     builtPassword += randomCharacter;
   }
-  return (builtPassword)
+  
+  //
+  // validation for at least one of each selected option in the final password?
+  //
+  
+  // return local variable containing random characters
+  return builtPassword;
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  // prompt user for password options
   getPasswordOptions();
-  var thePassword = getRandom(useableCharacters);
-  console.log("thePassword is : "+thePassword);
+
+  // generate and return a random password based upon the user selected options
+  // The password is "returned" so that it is correctly handled and displayed in
+  // the provided code below.
+  return getRandom();
 }
+
+// The following is "core" provided code
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -180,8 +130,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// Generate a password when the "button" is clicked.
-// Once prompts are answered then the password should be generated and displayed in an alert or written to the page
-
-// Not produce any errors in the console when you inspect it using Chrome DevTools.
